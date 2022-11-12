7be1a121f02b1dcbc752a1c5ec7e40fd0a205b95
pablo2406 <118069434+pablo2406@users.noreply.github.com>
2022-11-12 18:24:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk #Zusatzbibliothek, um verschiede Bilddateiformate zu nutzen (python imaging library)
 
 from GUI import GUI
 from Spiel import Karte, Kartenhand, Spiel
 
-def main():
+def main(): #Erstellung von Spielfläche mit angegebenen Maßen
     
     root = tk.Tk()
     root.title("UNO Spiel")
     root.attributes('-fullscreen',True)
     root.geometry("1920x1080")
     
-    main_canvas = tk.Canvas(master=root, bg ='#006600')  
+    main_canvas = tk.Canvas(master=root, bg ='#006600') #Erstellung Canvas-Element um grafische Möglichkeiten zu haben
     main_canvas.pack(fill="both", expand=True)
 
-    gui = GUI(main_canvas)
+    gui = GUI(main_canvas) #Angeben wo sich die Gui befinden soll
     game = Spiel(6, gui)
 
     bilder_pro_sekunde = 60
-    game.spielen(root, int(1000 / bilder_pro_sekunde))
+    game.spielen(root, int(1000 / bilder_pro_sekunde)) #Angabe wie oft geupdatet wird (fps)
 
     root.mainloop()
 
